b7452b4407822a1fc8d0582f26f251957f13a37f
justdan111 <emmanuelngulube292@gmail.com>
2026-02-20 17:54:15 +0100

    feat: add theme toggle functionality with next-themes integration and create ThemeProvider component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SidebarProvider } from "@/components/sidebar-provider";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,8 +28,17 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-      <SidebarProvider>{children}</SidebarProvider>
+      ><ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+          
+      <SidebarProvider>
+        {children}
+        </SidebarProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
